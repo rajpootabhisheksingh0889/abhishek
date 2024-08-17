95446dfd13668e8e39c2b3930f6b4523ac9e00d2
Rajpoot Abhishek <167678105+rajpootabhisheksingh0889@users.noreply.github.com>
2024-08-17 12:28:48 +0530

    sidebar issue fixed

--- src/layouts/full/sidebar/MenuItems.js
+++ src/layouts/full/sidebar/MenuItems.js
@@ -195,3 +195,171 @@
 // };
 
 // export default GetMenuItems;
+import {
+    IconAperture, IconCopy, IconLayoutDashboard, IconLogin, IconMoodHappy, IconTypography, IconUserPlus
+} from '@tabler/icons';
+
+import { uniqueId } from 'lodash';
+
+const Menuitems = [
+    {
+        navlabel: true,
+        subheader: 'Home',
+    },
+
+    {
+        id: uniqueId(),
+        title: 'Dashboard',
+        icon: IconLayoutDashboard,
+        href: '/dashboard',
+    },
+
+    {
+        id: uniqueId(),
+        title: 'User List',
+        icon: IconLayoutDashboard,
+        href: '/userlist',
+    },
+    {
+        id: uniqueId(),
+        title: 'Agent List',
+        icon: IconLayoutDashboard,
+        href: '/agentlist',
+    },
+
+    {
+        id: uniqueId(),
+        title: 'Recording',
+        icon: IconLayoutDashboard,
+        href: '/recordinglist',
+    },
+
+    {
+        id: uniqueId(),
+        title: 'Product List',
+        icon: IconLayoutDashboard,
+        href: '/productlist',
+    },
+
+    {
+        id: uniqueId(),
+        title: 'Discount Coupon',
+        icon: IconLayoutDashboard,
+        href: '/discountlist',
+    },
+    {
+        id: uniqueId(),
+        title: 'Order Mangement',
+        icon: IconLayoutDashboard,
+        href: '/ordermangement',
+    },
+    {
+        id: uniqueId(),
+        title: 'Transaction Records',
+        icon: IconLayoutDashboard,
+        href: '/transactionrecords',
+    },
+
+    {
+        id: uniqueId(),
+        title: 'Permission',
+        icon: IconLayoutDashboard,
+        href: '/permission',
+    },
+];
+
+const Menuitems1 = [
+    {
+        navlabel: true,
+        subheader: 'Home',
+    },
+
+    {
+        id: uniqueId(),
+        title: 'Dashboard',
+        icon: IconLayoutDashboard,
+        href: '/dashboard',
+    },
+
+    {
+        id: uniqueId(),
+        title: 'Chat',
+        icon: IconLayoutDashboard,
+        href: '/chat',
+    },
+    {
+        id: uniqueId(),
+        title: 'Product',
+        icon: IconLayoutDashboard,
+        href: '/product',
+    },
+
+    {
+        id: uniqueId(),
+        title: 'Order History',
+        icon: IconLayoutDashboard,
+        href: '/orderhistory',
+    },
+
+    {
+        id: uniqueId(),
+        title: 'Subscription',
+        icon: IconLayoutDashboard,
+        href: '/subscription',
+    },
+
+
+];
+
+const Menuitems2 = [
+    {
+        navlabel: true,
+        subheader: 'Home',
+    },
+
+    {
+        id: uniqueId(),
+        title: 'Dashboard',
+        icon: IconLayoutDashboard,
+        href: '/dashboard',
+    },
+
+    {
+        id: uniqueId(),
+        title: 'Call History',
+        icon: IconLayoutDashboard,
+        href: '/callhistory',
+    },
+    {
+        id: uniqueId(),
+        title: 'Chat',
+        icon: IconLayoutDashboard,
+        href: '/Chat',
+    },
+
+    {
+        id: uniqueId(),
+        title: 'Reports',
+        icon: IconLayoutDashboard,
+        href: '/reports',
+    },
+
+
+];
+
+const getMenuItems = () => {
+    const userType = localStorage.getItem('user_type');
+    console.log(`User type: ${userType}`); // Debugging line
+    switch (userType) {
+        case 'AD':
+            return Menuitems;
+        case 'CU':
+            return Menuitems1;
+        case 'AG':
+            return Menuitems2;
+        default:
+            return []; // Always return an array
+    }
+};
+
+export default getMenuItems;
--- src/layouts/full/sidebar/SidebarItems.js
+++ src/layouts/full/sidebar/SidebarItems.js
@@ -1,32 +1,62 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+// import { useLocation } from 'react-router';
+// import { Box, List } from '@mui/material';
+// import NavItem from './NavItem';
+// import NavGroup from './NavGroup/NavGroup';
+// import axios from 'axios';
+
+// const SidebarItems = () => {
+//   const { pathname } = useLocation();
+//   const pathDirect = pathname;
+//   const [menuItems, setMenuItems] = useState([]);
+
+//   useEffect(() => {
+//     const fetchMenuItems = async () => {
+//       try {
+//         const uid = localStorage.getItem('uid');
+//         if (!uid) {
+//           throw new Error('User ID not found in localStorage');
+//         }
+
+//         const response = await axios.post('http://134.209.145.149:9999/api/checkSidebar', { uid });
+//         setMenuItems(response.data);
+//       } catch (error) {
+//         console.error('Failed to fetch menu items:', error);
+//       }
+//     };
+
+//     fetchMenuItems();
+//   }, []);
+
+//   return (
+//     <Box sx={{ px: 3 }}>
+//       <List sx={{ pt: 0 }} className="sidebarNav">
+//         {menuItems.map((item) => {
+//           if (item.subheader) {
+//             return <NavGroup item={item} key={item.subheader} />;
+//           } else {
+//             return (
+//               <NavItem item={item} key={item.id} pathDirect={pathDirect} />
+//             );
+//           }
+//         })}
+//       </List>
+//     </Box>
+//   );
+// };
+
+// export default SidebarItems;
+import React from 'react';
+import getMenuItems from './MenuItems'; // Ensure this path is correct
 import { useLocation } from 'react-router';
 import { Box, List } from '@mui/material';
 import NavItem from './NavItem';
 import NavGroup from './NavGroup/NavGroup';
-import axios from 'axios';
 
 const SidebarItems = () => {
   const { pathname } = useLocation();
   const pathDirect = pathname;
-  const [menuItems, setMenuItems] = useState([]);
-
-  useEffect(() => {
-    const fetchMenuItems = async () => {
-      try {
-        const uid = localStorage.getItem('uid');
-        if (!uid) {
-          throw new Error('User ID not found in localStorage');
-        }
-
-        const response = await axios.post('http://134.209.145.149:9999/api/checkSidebar', { uid });
-        setMenuItems(response.data);
-      } catch (error) {
-        console.error('Failed to fetch menu items:', error);
-      }
-    };
-
-    fetchMenuItems();
-  }, []);
+  const menuItems = getMenuItems(); // Dynamically get the menu items based on user type
 
   return (
     <Box sx={{ px: 3 }}>
